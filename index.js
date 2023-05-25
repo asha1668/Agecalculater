604bc7902823e5005cb7886e8ebfd71d924e0337
asha <ashabadavagol1668@gmail.com>
2023-05-25 14:44:48 +0530

    add file

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function calculateAge() {
+    var name = document.getElementById("name").value;
+    var lastName = document.getElementById("lastname").value;
+    var dob = document.getElementById("dob").value;
+    var currentDate = new Date();
+    var selectedDate = new Date(dob);
+    var age = currentDate.getFullYear() - selectedDate.getFullYear();
+    
+    
+    // Check if the birthday hasn't occurred yet this year
+    if (currentDate.getMonth() < selectedDate.getMonth() || 
+        (currentDate.getMonth() === selectedDate.getMonth() && 
+        currentDate.getDate() < selectedDate.getDate())) {
+      age--;
+    }
+    
+    document.getElementById("result").innerHTML = " Hi my name is " + name+ " " +lastName+ " Age is " +age;
+    
+
+    
+    
+    
+  }
